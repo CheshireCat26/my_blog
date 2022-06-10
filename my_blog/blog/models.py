3a266aaff4948e86834fc7fb0a992b048b9eabea
Nail <NailMinnigalinWork@gmail.com>
2022-06-10 14:55:33 +0300

    recently added articles has new badge

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from django.db import models
 
-
 # Create your models here.
+from django.utils import timezone
+
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +14,6 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def is_add_recently(self):
+        return timezone.now() - timedelta(days=7) < self.pub_date <= timezone.now()
