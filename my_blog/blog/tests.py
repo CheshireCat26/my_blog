e6ca6167c8293e916e7ae48aaf8c37a6adf42763
Nail <NailMinnigalinWork@gmail.com>
2022-06-13 22:54:44 +0300

    add test_is_add_recently_for_article_in_future

--- my_blog/blog/tests.py
+++ my_blog/blog/tests.py
@@ -11,3 +11,8 @@ class ArticleTestCase(TestCase):
         """Articles with pub_date > 7 days isn't add recently"""
         article = Article(pub_date=timezone.now() - timedelta(days=7, seconds=1))
         self.assertEqual(article.is_add_recently(), False)
+
+    def test_is_add_recently_for_article_in_future(self):
+        """Article with pub_date in future isn't add recently"""
+        article = Article(pub_date=timezone.now() + timedelta(seconds=1))
+        self.assertEqual(article.is_add_recently(), False)
