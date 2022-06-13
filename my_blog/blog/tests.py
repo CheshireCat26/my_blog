220b9c65736da03106b696045497de3f01e07def
Nail <NailMinnigalinWork@gmail.com>
2022-06-13 22:51:09 +0300

    add test_is_add_recently_for_article_in_long_past

--- my_blog/blog/tests.py
+++ my_blog/blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Article
+from datetime import timedelta
+from django.utils import timezone
+
 
 # Create your tests here.
+
+class ArticleTestCase(TestCase):
+    """Articles with pub_date > 7 days isn't add recently"""
+    def test_is_add_recently_for_article_in_long_past(self):
+        article = Article(pub_date=timezone.now() - timedelta(days=7, seconds=1))
+        self.assertEqual(article.is_add_recently(), False)
