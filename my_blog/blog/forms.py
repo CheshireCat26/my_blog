0e727eeaf6454359f6165feaae370f5c5f419cd5
Nail <NailMinnigalinWork@gmail.com>
2022-06-16 04:48:31 +0300

    Insert bootstrap

--- my_blog/blog/forms.py
+++ my_blog/blog/forms.py
@@ -1,15 +1,50 @@
 from django import forms
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control ",
+            }
+        ),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "new-password",
+                "class": "form-control"
+            }
+        ),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+        widgets = {
+            'username': forms.TextInput(
+                attrs={
+                    "class": "form-control"
+                }
+            ),
+        }
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
