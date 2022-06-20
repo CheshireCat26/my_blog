8d8c8c79ee495cee9cdfd0ea84703f9f37ac611a
Nail <NailMinnigalinWork@gmail.com>
2022-06-20 23:31:12 +0300

    UserVotes model

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -17,3 +17,9 @@ class Article(models.Model):
 
     def is_add_recently(self):
         return timezone.now() - timedelta(days=7) < self.pub_date <= timezone.now()
+
+
+class UsersVotes(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    positive = models.BooleanField()
